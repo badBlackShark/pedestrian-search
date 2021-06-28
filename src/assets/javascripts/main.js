0d764b07d2f5af7e10946e2675c16bb11cc4d392
badBlackShark <Timdaniel.Engelhardt@web.de>
2021-06-28 22:19:44 +0200

    Added imports for JS and CSS in correct locations.

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -1,3 +1,7 @@
 import 'bootstrap';
 import Amber from 'amber';
 import Logo from '../images/logo.svg';
+import './extract.js';
+import './remove_results.js';
+import './search.js';
+import '../stylesheets/index.css';
